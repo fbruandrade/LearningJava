b47996785e1067867063eb4b3d316fcd9efdde9b
fandrade <felipe.andrade@jetty.com.br>
2017-06-07 14:42:52 -0300

    Simple loop FOR example

---
+++ SimpleCode/ForDemo.java
@@ -0,0 +1,10 @@
+class ForDemo {
+	public static void main(String[] args) {
+		int count;
+
+		for (count = 0; count < 5; count = count + 1) {
+			System.out.println("Count = " + count);
+		}
+		System.out.println("Done!");
+	}
+} 
