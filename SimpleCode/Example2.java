4b59192122b8c6a819e20db8da6c483d623ec5ea
fandrade <felipe.andrade@jetty.com.br>
2017-06-07 14:40:29 -0300

    First Example, print a Hello World and working with some variables.

---
+++ SimpleCode/Example2.java
@@ -0,0 +1,13 @@
+class Example2 {
+	public static void main(String args[]) {
+		int var1;
+		int var2;
+
+		var1 = 1024;
+		System.out.println("Conteúdo da var1 é " + var1);
+
+		var2 = var1 / 2;
+		System.out.println("var2 = var1/2");
+		System.out.println(var2);
+	}
+}
